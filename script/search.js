28a14b8468979459da69a45ebd040a9f2d676712
krithika117 <krithikanithyanandam@gmail.com>
2022-08-25 08:54:34 +0530

    search changes

--- script/search.js
+++ script/search.js
@@ -2,6 +2,35 @@ var server = "http://127.0.0.1:5000/";
 
 $(document).ready(function () {
     //Create API Call
+    $.ajax({
+        method: "POST",
+        url: server + 'show/places',
+        contentType: 'application/json;charset=UTF-8',
+        dataType: 'json',
+        success: function (data) {
+            $('tbody').empty();
+            console.log(data.response.placeId)
+            for (var i = 0; i < data.response.length; i++) {
+                var row = ''
+                row = $('<tr><td>' +
+                    data.response[i].placeId +
+                    '</td><td>' +
+                    data.response[i].location +
+                    '</td><td>' +
+                    data.response[i].address +
+                    '</td> ' +
+                    // '<a class="btn btn-primary" id="' + data.response[i].placeId + '" href="book.php" role="button">Login</a>'
+                    '</tr>'
+                );
+                $('tbody').append(row);
+            }
+
+        },
+        error: function (err) {
+            alert("Something went wrong")
+        }
+    });
+
     $('#search').on('click', function (e) {
         e.preventDefault();
         var placename = document.getElementById("placename").value;
@@ -10,6 +39,7 @@ $(document).ready(function () {
             alert("Please fill Service Name.");
 
         } else {
+
             $.ajax({
                 method: "POST",
                 url: server + 'search',
@@ -20,7 +50,6 @@ $(document).ready(function () {
                 success: function (data) {
                     $('tbody').empty();
                     console.log(data.response.placeId)
-
                     for (var i = 0; i < data.response.length; i++) {
                         var row = ''
                         row = $('<tr><td>' +
@@ -45,6 +74,6 @@ $(document).ready(function () {
 
     });
 
-
+  
 
 })
--- search.php
+++ search.php
@@ -8,12 +8,12 @@
     </head>
 
 <body>
-    <div class="mb-3 col-7  mb-3 col-7">
+    <a type="button" href="book.php" role="button" style="float:right;">Book your wash now!</a>
     <h1 class="display-5">Branches around</h1>
-    <a class="btn btn-primary" href="book.php" role="button">Book your wash now!</a>
+
     <input class="form-control" id="placename" placeholder="Type to search...">
     <input type="button" id="search" value="Search">
-    <table class="table  mb-3 col-7  mb-3 col-7">
+    <table class="table ">
         <thead>
             <tr>
                 <th>Place ID</th>
@@ -24,7 +24,7 @@
         <tbody>
         </tbody>
     </table>
-    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
